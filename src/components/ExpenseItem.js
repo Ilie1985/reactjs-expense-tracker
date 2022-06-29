c80d8be0909847f62efd33bd64329625b7863630
Marian Ilie <ilie.marian32@yahoo.com>
2022-06-29 10:40:24 +0100

    date built in methods

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "../components/expenseItem.css";
 
-
-
 //access the data needeed from App.js with the help of props
-
-
+//use a built in method which is accesible on all date objects in javascript-> toLocaleString() to output the date in a readable format
+//toLocaleString() holds two arguments, first argument is the language "en-US", second argument is an object wher is configured how specifically that date should be formated {month:"long"} TO OUTPUT THE MONTH
+//toLocaleString() holds two arguments, first argument is the language "en-US", second argument is an object wher is configured how specifically that date should be formated {day:"2-digit"} TO OUTPUT THE DAY
+//TO OUTPUT THE YEAR use the getFullYear built in method
 
 const ExpenseItem = (props) => {
- 
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
+  const year = props.date.getFullYear();
   return (
     <div className={"expense-item"}>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
 
       <div className={"expense-item_description"}>
         <h2>{props.title}</h2>
