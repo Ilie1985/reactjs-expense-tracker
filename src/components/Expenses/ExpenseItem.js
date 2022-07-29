ef1b4457b55a697bac2ff71981c61a7858ef28b9
Marian Ilie <ilie.marian32@yahoo.com>
2022-07-29 09:36:32 +0100

    conditional rendering of the form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -15,22 +15,24 @@ const ExpenseItem = (props) => {
   // };
 
   return (
-    <Card className={"expense-item"}>
-      <ExpenseDate date={props.date} />
+    <li>
+      <Card className={"expense-item"}>
+        <ExpenseDate date={props.date} />
 
-      <div className={"expense-item_description"}>
-        {/* <h2>{changeTitle}</h2> */}
-        <h2>{props.title}</h2>
-        <div className={"expense-item_price"}>£{props.amount}</div>
-      </div>
-      {/* <button
+        <div className={"expense-item_description"}>
+          {/* <h2>{changeTitle}</h2> */}
+          <h2>{props.title}</h2>
+          <div className={"expense-item_price"}>£{props.amount}</div>
+        </div>
+        {/* <button
         onClick={() => {
           clickHandler();
         }}
       >
         Change title
       </button> */}
-    </Card>
+      </Card>
+    </li>
   );
 };
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ExpenseItem from "./ExpenseItem";
+
 import "./expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [expenseFilteredYear, setExpenseFilteredYear] = useState("2022");
@@ -16,6 +17,8 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === expenseFilteredYear;
   });
 
+  
+
   return (
     <div>
       <Card className={"expenses"}>
@@ -23,55 +26,10 @@ const Expenses = (props) => {
           selectedYear={expenseFilteredYear}
           onYearChange={changedYear}
         />
-       
-       
-        {filteredExpenses.length === 0 && <p>No expenses found .</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
-     
-     
+        <ExpensesList items ={filteredExpenses}/>
       </Card>
 
-      {/*        
- {filteredExpenses.map((expense) => {
-   return (
-     <ExpenseItem
-       key={expense.id}
-       title={expense.title}
-       amount={expense.amount}
-       date={expense.date}
-     />
-   );
- })}
-  */}
-
-      {/* <ExpenseItem
-   title={props.items[0].title}
-   amount={props.items[0].amount}
-   date={props.items[0].date}
- />
- <ExpenseItem
-   title={props.items[1].title}
-   amount={props.items[1].amount}
-   date={props.items[1].date}
- />
- <ExpenseItem
-   title={props.items[2].title}
-   amount={props.items[2].amount}
-   date={props.items[2].date}
- />
- <ExpenseItem
-   title={props.items[3].title}
-   amount={props.items[3].amount}
-   date={props.items[3].date}
- /> */}
+    
     </div>
   );
 };
